b682df39b17149e4405ff52aac4450f257e0d054
aliakbarMor <aliakbarmoradhaseli@gmail.com>
2020-01-19 20:08:55 +0330

    v1 bug fixed

--- app/src/main/java/com/example/mymusic/viewModel/MusicItemViewModel.kt
+++ app/src/main/java/com/example/mymusic/viewModel/MusicItemViewModel.kt
@@ -24,57 +24,50 @@ class MusicItemViewModel(music: Music) : ViewModel() {
     var duration: String? = music.duration
 
     companion object {
-        private val metaRetriever = MediaMetadataRetriever()
-        private var LOCK = Any()
 
-        var count = 0
-
-        @RequiresApi(Build.VERSION_CODES.P)
         @JvmStatic
         @BindingAdapter("app:loadImg")
         fun loadImage(imageView: ImageView, path: String) {
-            if (count % 2 == 0) {
-                Thread(Runnable {
-                    synchronized(LOCK) {
-                        metaRetriever.setDataSource(path)
-                        val art = metaRetriever.embeddedPicture
-                        if (art != null) {
-                            val songImage = BitmapFactory.decodeByteArray(art, 0, art.size)
-                            val bitmapRequestBuilder = Glide
-                                .with(imageView.context)
-                                .asBitmap()
-                                .override(100, 100)
-                                .load(songImage)
-                                .circleCrop()
-                            imageView.post { bitmapRequestBuilder.into(imageView) }
-                        } else {
-                            imageView.post { imageView.setImageResource(R.drawable.ic_music) }
-                        }
-                    }
-                }).start()
-                count++
-            } else {
-                Thread(Runnable {
-                    synchronized(LOCK) {
-                        metaRetriever.setDataSource(path)
-                        val art = metaRetriever.embeddedPicture
-                        if (art != null) {
-                            val songImage = BitmapFactory.decodeByteArray(art, 0, art.size)
-                            val bitmapRequestBuilder = Glide
-                                .with(imageView.context)
-                                .asBitmap()
-                                .override(100, 100)
-                                .load(songImage)
-                                .circleCrop()
-                            imageView.post { bitmapRequestBuilder.into(imageView) }
-                        } else {
-                            imageView.post { imageView.setImageResource(R.drawable.ic_music) }
-                        }
-                    }
-                }).start()
-                count++
-            }
+            Thread(Runnable {
+                val metaRetriever = MediaMetadataRetriever()
+                metaRetriever.setDataSource(path)
+                val art = metaRetriever.embeddedPicture
+                if (art != null) {
+                    val songImage = BitmapFactory.decodeByteArray(art, 0, art.size)
+                    val bitmapRequestBuilder = Glide
+                        .with(imageView.context)
+                        .asBitmap()
+                        .override(100, 100)
+                        .load(songImage)
+                        .circleCrop()
+                    imageView.post { bitmapRequestBuilder.into(imageView) }
+                } else {
+                    imageView.post { imageView.setImageResource(R.drawable.ic_music) }
+                }
+            }).start()
         }
+
+        @JvmStatic
+        @BindingAdapter("app:loadHorizontalIcons")
+        fun loadHorizontalIcons(imageView: ImageView, path: String) {
+            Thread(Runnable {
+                val metaRetriever = MediaMetadataRetriever()
+                metaRetriever.setDataSource(path)
+                val art = metaRetriever.embeddedPicture
+                if (art != null) {
+                    val songImage = BitmapFactory.decodeByteArray(art, 0, art.size)
+                    val bitmapRequestBuilder = Glide
+                        .with(imageView.context)
+                        .asBitmap()
+                        .override(300, 300)
+                        .load(songImage)
+                    imageView.post { bitmapRequestBuilder.into(imageView) }
+                } else {
+                    imageView.post { imageView.setImageResource(R.drawable.ic_music) }
+                }
+            }).start()
+        }
+
     }
 
 
